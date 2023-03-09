15f9f030d60e1d4893a225a4fc127eeb7c9b82e8
unknown <sebastesb1986@gmail.com>
2023-03-08 19:02:09 -0500

    Base project

--- resources/views/weather/index.blade.php
+++ resources/views/weather/index.blade.php
@@ -11,6 +11,9 @@
             height: 400px; /* The height is 400 pixels */
             width: 100%; /* The width is the width of the web page */
         }
+        td {
+            text-align: center;
+        }
     </style>
 
     </head>
